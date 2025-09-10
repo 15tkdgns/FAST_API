2d914be44e4a30888e86966be3ae7934432b59fc
WM730 <rlaals876@naver.com>
2025-09-11 07:14:18 +0900

    Update order.py

---
+++ routers/order.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, Path, Query
+from sqlalchemy.orm import Session
+from crud.orders import OrderCrud
+from schemas import OrderCreate
+from database import get_db
+
+router = APIRouter(prefix="/order", tags=["Orders"])
+
+@router.post("/")
+def create_order(order: OrderCreate, db: Session = Depends(get_db)):
+return OrderCrud.create_order(order, db) # 생성
+
+@router.get("/{order_id}")
+def get_order(order_id: int = Path(..., ge=1), db: Session = Depends(get_db)):
+return OrderCrud.get_order(order_id, db) # 단건 조회
+
+@router.put("/{order_id}")
+def update_order_price(
+order_id: int = Path(..., ge=1),
+price: float = Query(..., gt=0),
+db: Session = Depends(get_db),
+):
+return OrderCrud.update_order_price(order_id, price, db) # 가격 수정
+
+@router.delete("/{order_id}")
+def delete_order(order_id: int = Path(..., ge=1), db: Session = Depends(get_db)):
+return OrderCrud.delete_order(order_id, db) # 삭제
+
