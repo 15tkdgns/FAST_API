32a763de8dd15dcbd05c9d7cf848763801b6ea04
WM730 <rlaals876@naver.com>
2025-09-11 16:03:16 +0900

    Update review.py

--- routers/review.py
+++ routers/review.py
@@ -6,22 +6,22 @@ from database import get_db
 
 router = APIRouter(prefix="/review", tags=["Reviews"])
 
-@router.post("/")
+@router.post("/create")
 def create_review(review: ReviewCreate, db: Session = Depends(get_db)):
   return ReviewCrud.create_review(review, db) # 생성
 
-@router.get("/")
+@router.get("/list")
 def get_reviews(db: Session = Depends(get_db)):
   return ReviewCrud.get_reviews(db) # 전체 조회
 
-@router.put("/{review_id}")
+@router.put("/update/{review_id}")
 def update_review(
-review_id: int = Path(..., ge=1),
-update: ReviewCreate | None = None,
-db: Session = Depends(get_db),
+  review_id: int = Path(..., ge=1),
+  update: ReviewCreate | None = None,
+  db: Session = Depends(get_db),
 ):
   return ReviewCrud.update_review(review_id, update, db) # 수정
 
-@router.delete("/{review_id}")
+@router.delete("/delete/{review_id}")
 def delete_review(review_id: int = Path(..., ge=1), db: Session = Depends(get_db)):
   return ReviewCrud.delete_review(review_id, db) # 삭제
