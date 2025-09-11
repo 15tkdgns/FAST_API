072bd3c5dd7a0b956d9616d437eaa497d27e1bcb
WM730 <rlaals876@naver.com>
2025-09-11 16:03:46 +0900

    Update user.py

--- routers/user.py
+++ routers/user.py
@@ -7,22 +7,22 @@ from database import get_db
 router = APIRouter(prefix="/user",tags=['Users'])
 
 # post
-@router.post("/")
+@router.post("/create")
 def create_user(user:UserCreate, db:Session = Depends(get_db)):
   return UserCrud.create_user(user, db)
 
 # get / 유저 id 값 가져와서 조회
-@router.get("/{user_id}")
+@router.get("/list/{user_id}")
 def get_user(user_id:int=Path(..., ge=1), db:Session = Depends(get_db)):
   return UserCrud.get_user(user_id, db)
 
 # put / 유저 id 값 가져와서 수정
-@router.put("/{user_id}")
+@router.put("/update/{user_id}")
 def update_user(user_id:int = Path(..., ge=1), update:UserCreate|None=None, db:Session = Depends(get_db)):
   return UserCrud.update_user(user_id, update, db)
 
 # @router.delete("/")
-@router.delete("/", summary="유저 삭제 (username 쿼리) ")
+@router.delete("/delete", summary="유저 삭제 (username 쿼리) ")
 def delete_user(
   username: str = Query(..., min_length=1, description="삭제할 유저의 username"),
   db: Session = Depends(get_db),
